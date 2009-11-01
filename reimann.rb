b0d8b46e75f7c303a47c7f115a999fe7a8328b64
David Masover <ninja@slaphack.com>
2009-11-01 12:46:14 -0600

    Some simplifications...

--- reimann.rb
+++ reimann.rb
@@ -9,18 +9,18 @@ module Math
     right = a + delta
     right_sum = (right_value = yield right)
     mid = a + (delta/2)
-    mid_sum = (mid_value = yield mid)
+    mid_sum = yield mid
     # So this is precise -- right == b when it's supposed to,
     # because all x-values are kept as integers or rationals.
     while right < b
       mid += delta
-      mid_sum += (mid_value = yield mid)
+      mid_sum += yield mid
       right += delta
       right_sum += (right_value = yield right)
     end
     # Given either the left sum or the right sum, we can
     # simply pop one off the right and add one to the left.
-    left_sum = (right_sum - right_value + yield(a))
+    left_sum = right_sum - right_value + yield(a)
     [left_sum*delta, mid_sum*delta, right_sum*delta]
   end
 end
