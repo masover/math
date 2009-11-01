e5fd073fd4a05837ef86b79d8176f4e571fee013
David Masover <ninja@slaphack.com>
2009-11-01 10:10:19 -0600

    Calculates Reimann sums from left/mid/right points.

---
+++ reimann.rb
@@ -0,0 +1,24 @@
+module Math
+  def self.reimann a, b, n
+    if a > b
+      a,b = b,a
+    end
+    delta = Rational(b-a,n)
+    right = a + delta
+    right_sum = (right_value = yield right)
+    mid = a + (delta/2)
+    mid_sum = (mid_value = yield mid)
+    # So this is precise -- right == b when it's supposed to,
+    # because all x-values are kept as integers or rationals.
+    while right < b
+      mid += delta
+      mid_sum += (mid_value = yield mid)
+      right += delta
+      right_sum += (right_value = yield right)
+    end
+    # Given either the left sum or the right sum, we can
+    # simply pop one off the right and add one to the left.
+    left_sum = (right_sum - right_value + yield(a))
+    [left_sum*delta, mid_sum*delta, right_sum*delta]
+  end
+end
